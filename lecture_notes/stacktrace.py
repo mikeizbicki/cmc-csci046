c5bf5e4c0e816ae5a64e5a3e32cfe4d9b86ac51e
Mike Izbicki <mike@izbicki.me>
2020-02-20 08:30:29 -0800

    fix lecture notes

--- lecture_notes/stacktrace.py
+++ lecture_notes/stacktrace.py
@@ -37,3 +37,53 @@ def recursive(n):
         recursive(n-1)
 
 recursive(5)
+
+'''
+The output of `python3 stacktrace.py` is:
+
+----------------------------------------
+  File "stacktrace.py", line 20, in <module>
+    function_a()
+  File "stacktrace.py", line 8, in function_a
+    traceback.print_stack()
+----------------------------------------
+  File "stacktrace.py", line 23, in <module>
+    function_b()
+  File "stacktrace.py", line 11, in function_b
+    function_a()
+  File "stacktrace.py", line 8, in function_a
+    traceback.print_stack()
+----------------------------------------
+  File "stacktrace.py", line 26, in <module>
+    function_c()
+  File "stacktrace.py", line 14, in function_c
+    function_b()
+  File "stacktrace.py", line 11, in function_b
+    function_a()
+  File "stacktrace.py", line 8, in function_a
+    traceback.print_stack()
+----------------------------------------
+  File "stacktrace.py", line 29, in <module>
+    function_d()
+  File "stacktrace.py", line 17, in function_d
+    function_c()
+  File "stacktrace.py", line 14, in function_c
+    function_b()
+  File "stacktrace.py", line 11, in function_b
+    function_a()
+  File "stacktrace.py", line 8, in function_a
+    traceback.print_stack()
+----------------------------------------
+  File "stacktrace.py", line 39, in <module>
+    recursive(5)
+  File "stacktrace.py", line 37, in recursive
+    recursive(n-1)
+  File "stacktrace.py", line 37, in recursive
+    recursive(n-1)
+  File "stacktrace.py", line 37, in recursive
+    recursive(n-1)
+  File "stacktrace.py", line 37, in recursive
+    recursive(n-1)
+  File "stacktrace.py", line 35, in recursive
+    traceback.print_stack()
+'''
