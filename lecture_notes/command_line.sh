cf250f32d7acca258439384d54f1501394bad8fa
Mike Izbicki <mike@izbicki.me>
2020-03-10 15:47:30 -0700

    lecture notes

---
+++ lecture_notes/command_line.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# files ending in .sh
+# or with the /bin/sh shebang
+# are called shell scripts
+
+./command_line.py
+./command_line.py
+./command_line.py
+./command_line.py
+
+./map --input_file=/data/twitter\ dataset/file1.zip
+./map --input_file=/data/twitter\ dataset/file1.zip
+./map --input_file=/data/twitter\ dataset/file1.zip
+./map --input_file=/data/twitter\ dataset/file1.zip
+./map --input_file=/data/twitter\ dataset/file1.zip
+./map --input_file=/data/twitter\ dataset/file1.zip
+
+./reduce.py
+
+# ls -l -a
+# ls -a -l
+# ls -la
+# ls -al
